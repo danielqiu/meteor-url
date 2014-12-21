a2b4b472d3a0486b1ddf36b5bddde0f72628dda4
Daniel Qiu <daniel@danielqiu.com>
2014-12-20 17:56:09 -0800

    package works on server side only

--- package.js
+++ package.js
@@ -2,17 +2,17 @@ Package.describe({
 	name: "mrt:url",
 	summary: "url npm library wrapped in meteor",
 	author: "Daniel Qiu",
-	version: "0.0.2",
+	version: "0.0.3",
 	git: "https://github.com/danielqiu/meteor-url.git"
+});
+
+Package.on_use(function(api){
+	api.versionsFrom('1.0');
+
+	api.add_files(['url.js'], 'server');
+	
+	api.export('url', 'server');
 });
 
 Npm.depends({url: "0.10.1"});
 
-Package.on_use(function(api){
-	api.versionsFrom('METEOR@0.9.1.1');
-
-	api.add_files(['url.js'], 'server', 'client');
-	
-	if(api.export)
-		api.export('url');
-});
